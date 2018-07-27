a81e4b2c975ad2376172e9356e4740efdb5d38ae
Darko <darkoatanasovski@gmail.com>
2018-07-27 23:30:52 +0200

    gofmt

--- striptags.go
+++ striptags.go
@@ -1,53 +1,53 @@
 package htmltags
 
 import (
-	"strings"
 	"bytes"
 	"golang.org/x/net/html"
-	)
+	"strings"
+)
 
-type Nodes struct{
+type Nodes struct {
 	Elements *html.Node
 }
 
 //Strip tags from provided html string
-func Strip(content string, allowedTags []string, stripInlineAttributes bool) (Nodes, error){
+func Strip(content string, allowedTags []string, stripInlineAttributes bool) (Nodes, error) {
 	document, err := toNodes(content)
 	handleError(err)
 	var nodeTree html.Node
 
 	var output func(document *html.Node, nt *html.Node)
-	output = func(document *html.Node, nt *html.Node){
+	output = func(document *html.Node, nt *html.Node) {
 		for c := document.FirstChild; c != nil; c = c.NextSibling {
-			if c.Type == html.TextNode || ( c.Type == html.ElementNode && inArray(c.Data, allowedTags)) {
+			if c.Type == html.TextNode || (c.Type == html.ElementNode && inArray(c.Data, allowedTags)) {
 				var childNode html.Node
 				childNode.Type = c.Type
 				childNode.Data = c.Data
 				if stripInlineAttributes == true {
 					childNode.Attr = []html.Attribute{}
-				}else{
+				} else {
 					childNode.Attr = c.Attr
 				}
 				nt.AppendChild(&childNode)
 				output(c, nt.LastChild)
-			}else{
+			} else {
 				output(c, nt)
 			}
 		}
 	}
 	output(document, &nodeTree)
-	return Nodes{Elements:&nodeTree}, nil
+	return Nodes{Elements: &nodeTree}, nil
 }
 
 //String to nodes helper.
-func toNodes(document string) (*html.Node, error){
+func toNodes(document string) (*html.Node, error) {
 	nodes, err := html.Parse(strings.NewReader(html.UnescapeString(document)))
 	handleError(err)
 	return nodes, nil
 }
 
 //Nodes method. Converts nodes to string
-func (nodes *Nodes) ToString() string{
+func (nodes *Nodes) ToString() string {
 	var buf bytes.Buffer
 	for n := nodes.Elements.FirstChild; n != nil; n = n.NextSibling {
 		html.Render(&buf, n)
@@ -66,7 +66,7 @@ func inArray(a string, list []string) bool {
 }
 
 //Show error
-func handleError(err error){
+func handleError(err error) {
 	if err != nil {
 		panic(err)
 	}
